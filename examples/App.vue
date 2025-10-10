56ed1b32fd3af5fd6424989c35f65e80d71ab506
hugoxian <hugoxian@astribot.com>
2025-10-10 11:24:58 +0800

    first commit

---
+++ examples/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Glint UI Examples</h1>
+    <p>Welcome to Glint UI examples</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 这里可以导入并展示你的UI组件示例
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
